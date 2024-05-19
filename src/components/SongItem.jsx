b71b1c748c33b7cbbc5658f2206ca831cbaaee91
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 20:21:12 +0300

    feat: implement songItem - ui

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,5 +1,12 @@
-function SongItem() {
-  return <div>SongItem</div>;
+/* eslint-disable react/prop-types */
+function SongItem({ name, image, desc, id }) {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer transition duration-300 hover:bg-[#ffffff26] '>
+      <img src={image} alt={name} className='rounded' />
+      <p className='font-bold mt-2 mb-1'>{name}</p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  );
 }
 
 export default SongItem;
