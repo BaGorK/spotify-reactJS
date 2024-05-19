eb87df9e4e6dc9ecc2c3fe5e65f0fce7e3216596
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 10:06:32 +0300

    feat: player.jsx | add song image | name | desc

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,17 @@
+import { assets, songsData } from '../assets/assets';
+
 function Player() {
-  return <div className='text-white'>Player</div>;
+  return (
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+      <div className='hidden lg:flex items-center gap-4'>
+        <img src={songsData[0].image} alt='image' className='w-12' />
+        <div>
+          <p>{songsData[0].name}</p>
+          <p>{songsData[0].desc.slice(0, 15)}. . .</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Player;
