abca82be3f3fe660bc77c161a6caa484a2a7696b
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 19:45:01 +0300

    feat: display albumItems

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,5 +1,24 @@
+import { albumsData } from '../assets/assets';
+import AlbumItem from './AlbumItem';
+import NavBar from './NavBar';
+
 function DisplayHome() {
-  return <div>DisplayHome</div>;
+  return (
+    <>
+      <NavBar />
+      <div className='my-5 font-bold text-2xl'>
+        {albumsData.map((item, i) => (
+          <AlbumItem
+            key={i}
+            image={item.image}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default DisplayHome;
