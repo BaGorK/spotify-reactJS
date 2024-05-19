8f5b3c37ad6341019ffafd55741e35b3f4448971
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 19:38:11 +0300

    feat: implement AlbumItem - ui

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,5 +1,12 @@
-function AlbumItem() {
-  return <div>AlbumItem</div>;
+/* eslint-disable react/prop-types */
+function AlbumItem({ image, name, desc, id }) {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+      <img src={image} alt={image} className='rounded' />
+      <p className='font-bold mt-2 mb-1'>{name}</p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  );
 }
 
 export default AlbumItem;
