162f6a417ed9a38e95442a69f8139df17e6377b9
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 10:07:04 +0300

    feat: player.jsx | ui add play controls

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,6 +10,42 @@ function Player() {
           <p>{songsData[0].desc.slice(0, 15)}. . .</p>
         </div>
       </div>
+      <div className='flex flex-col items-center gap-1 m-auto'>
+        <div className='flex gap-4'>
+          <img
+            className='w-4 cursor-pointer'
+            src={assets.shuffle_icon}
+            alt='shuffle icon'
+          />
+          <img
+            className='w-4 cursor-pointer'
+            src={assets.prev_icon}
+            alt='shuffle icon'
+          />
+          <img
+            className='w-4 cursor-pointer'
+            src={assets.play_icon}
+            alt='shuffle icon'
+          />
+          <img
+            className='w-4 cursor-pointer'
+            src={assets.next_icon}
+            alt='shuffle icon'
+          />
+          <img
+            className='w-4 cursor-pointer'
+            src={assets.loop_icon}
+            alt='shuffle icon'
+          />
+        </div>
+        <div className='flex items-center gap-5'>
+          <p>1:06</p>
+          <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
+          </div>
+          <p>3:06</p>
+        </div>
+      </div>
     </div>
   );
 }
