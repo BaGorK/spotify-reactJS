14afdccfe75ce5918bd8aaebb83524acfefea302
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 20:24:10 +0300

    feat: add SongsItems on DisplayHome.jsx

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,6 +1,7 @@
-import { albumsData } from '../assets/assets';
+import { albumsData, songsData } from '../assets/assets';
 import AlbumItem from './AlbumItem';
 import NavBar from './NavBar';
+import SongItem from './SongItem';
 
 function DisplayHome() {
   return (
@@ -11,6 +12,20 @@ function DisplayHome() {
         <div className='flex overflow-auto'>
           {albumsData.map((item, i) => (
             <AlbumItem
+              key={i}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <div className='flex overflow-auto'>
+          {songsData.map((item, i) => (
+            <SongItem
               key={i}
               image={item.image}
               name={item.name}
