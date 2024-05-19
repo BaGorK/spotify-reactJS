e606da40f6887bbda644ab816b01161b61a1a769
BaGorK <edmealemkassahun@gmail.com>
2024-05-19 19:49:56 +0300

    feat: AlbumItem.jsx | add transition

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function AlbumItem({ image, name, desc, id }) {
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer transition duration-300 hover:bg-[#ffffff26]'>
       <img src={image} alt={image} className='rounded' />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
