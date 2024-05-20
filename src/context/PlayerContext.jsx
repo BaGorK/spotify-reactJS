efc60cb8a771165164ea5c18d81a8b9ed5917d2f
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 12:54:34 +0300

    feat: update player context

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,11 +1,39 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useRef, useState } from 'react';
+import { songsData } from '../assets/assets';
 
 const PlayerContext = createContext();
 
 export default function PlayerContextProvider({ children }) {
-  const contextValue = {};
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
+    },
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
+
+  const contextValue = {
+    audioRef,
+    seekBg,
+    seekBar,
+    track,
+    setTrack,
+    playStatus,
+    setPlayStatus,
+    time,
+    setTime,
+  };
 
   return (
     <PlayerContext.Provider value={contextValue}>
