7b160d2d894a72f7893d18afd4193592dbb843c4
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 11:09:12 +0300

    feat: update display routes

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import DisplayHome from './DisplayHome';
+import DisplayAlbum from './DisplayAlbum';
 
 function Display() {
   return (
     <div className='w-[100%] lg:ml-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%]'>
       <Routes>
         <Route path='/' element={<DisplayHome />} />
+        <Route path='/album/:id' element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
