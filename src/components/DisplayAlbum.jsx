9edc22d787d5505f3ddfbd9dacbbc27a36c0def7
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 11:53:15 +0300

    feat: display album items

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import NavBar from './NavBar';
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
 
 function DisplayAlbum() {
   const { id } = useParams();
@@ -37,6 +37,22 @@ function DisplayAlbum() {
         <p className='hidden sm:block'>Date Added</p>
         <img src={assets.clock_icon} alt='' className='m-auto w-4' />
       </div>
+      <hr />
+      {songsData.map((item, i) => (
+        <div
+          key={i}
+          className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] transition duration-300 hover:bg-[#ffffff2b] cursor-pointer'
+        >
+          <p className='text-white'>
+            <b className='mr-4 text-[#a7a7a7]'>{i + 1}</b>
+            <img src={item.image} alt='' className='inline w-10 mr-5' />
+            {item.name}
+          </p>
+          <p className='text-[15px]'>{albumData.name}</p>
+          <p className='text-[15px] hidden sm:block'>5 days ago</p>
+          <p className='text-[15px] text-center'>{item.duration}</p>
+        </div>
+      ))}
     </>
   );
 }
