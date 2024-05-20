f9e53930b37171e2a774bc98d1e69b58cce653a1
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 14:31:28 +0300

    feat: play when card click | songItem.jsx updated

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,7 +1,14 @@
+import { usePlayerContext } from '../context/PlayerContext';
+
 /* eslint-disable react/prop-types */
 function SongItem({ name, image, desc, id }) {
+  const { playWithId } = usePlayerContext();
+
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer transition duration-300 hover:bg-[#ffffff26] '>
+    <div
+      onClick={() => playWithId(id)}
+      className='min-w-[180px] p-2 px-3 rounded cursor-pointer transition duration-300 hover:bg-[#ffffff26] '
+    >
       <img src={image} alt={name} className='rounded' />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
