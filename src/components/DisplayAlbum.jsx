7316c369dd9cd7d26a5205813d030dbdc0258ef0
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 11:38:11 +0300

    feat: displayalbum | add a header for a grid

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -29,6 +29,14 @@ function DisplayAlbum() {
           </p>
         </div>
       </div>
+      <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+        <p>
+          <b className='mr-4'>#</b> Title
+        </p>
+        <p>Album</p>
+        <p className='hidden sm:block'>Date Added</p>
+        <img src={assets.clock_icon} alt='' className='m-auto w-4' />
+      </div>
     </>
   );
 }
