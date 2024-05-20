65d1171208ad81281ff99f44726553bab469312c
BaGorK <edmealemkassahun@gmail.com>
2024-05-20 14:57:32 +0300

    feat: update the sidebar | navigate to the home page when home_icon clicked

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
 function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <div className='w-[25%] h-full flex-col gap-2 text-white hidden lg:flex'>
       <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
-        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+        <div
+          onClick={() => navigate('/')}
+          className='flex items-center gap-3 pl-8 cursor-pointer'
+        >
           <img className='w-6' src={assets.home_icon} alt='home icon' />
           <p className='font-bold'>Home</p>
         </div>
